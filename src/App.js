dbca2cddf39ecd15a6d803fbcefaeecd2a84d059
BadisHammadache <badisgithub@gmail.com>
2022-11-18 00:56:55 +0100

    Initialisation de la page projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import PageLayout from "./layouts/pageLayout";
 import Home from "./pages/home/home";
 import LoginPage from "./pages/Login/LoginPage";
+import Projects from "./pages/Project/Projects";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<PageLayout />} >
           <Route index element={<Home />} />
           <Route path="home" element={<div>RIEN EIRNE</div>} />
+          <Route path="projects" element={<Projects />} />
         </Route>
         <Route path="*" element={<h1> ERROR 404 PAGE NOT FOUND  </h1>} />
       </Routes>
---
+++ src/pages/Project/Projects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <div>Projects Initialisation</div>;
+};
+
+export default Projects;
